efe6b94085dfd95c634e11479126b6dbe586ad86
Farit Mutugullin <farit.mutugullin@gmail.com>
2020-02-24 16:11:51 +0300

    Added main package with usage example

---
+++ main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	opb "github.com/cheetahfm/operators-between-digits"
+)
+
+func main() {
+	expectedResult := 200
+	var digits = opb.ExpressionDigits{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	var signs = opb.ExpressionSigns{'_', '+', '-'}
+
+	allExpressions := make(chan string)
+	go opb.GenAllExpressions(allExpressions, digits, signs)
+
+	validExpressions := make(chan string)
+	go opb.CheckExpressions(allExpressions, validExpressions, expectedResult)
+
+	fmt.Printf("Valid expressions with result %d\n", expectedResult)
+	opb.PrintValidResults(validExpressions)
+}
