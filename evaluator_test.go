70d90dc912d2e36aadd19c2d449672193388ab85
Farit Mutugullin <farit.mutugullin@gmail.com>
2020-02-24 16:20:10 +0300

    Added test for expression evaluator function

---
+++ evaluator_test.go
@@ -0,0 +1,26 @@
+package operators_between_digits_test
+
+import (
+	opb "github.com/cheetahfm/operators-between-digits"
+	"testing"
+)
+
+func TestCalculateExpression(t *testing.T) {
+	testCases := []struct {
+		expression string
+		answer     int
+	}{
+		{"1+2", 3},
+		{"12-42", -30},
+		{"324534", 324534},
+		{"12+0", 12},
+		{"12-0", 12},
+	}
+
+	for _, tc := range testCases {
+		result := opb.CalculateExpression(tc.expression)
+		if result != tc.answer {
+			t.Errorf("Expected %s=%d, but got %d", tc.expression, tc.answer, result)
+		}
+	}
+}
